dac81edba8f0580ac9b72bc02cb5ed250f8bf7b9
PavonEvelin <evelinluz17@gmail.com>
2021-08-27 13:40:59 -0300

    problema 1 ok

---
+++ TP1/WebApp_Problema1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_Problema1.Models;
+
+namespace WebApp_Problema1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public string Problema1(string num)
+        {
+            try
+            {
+                int num_aux = Convert.ToInt32(num);
+                return $"El cuadrado de {num} es: {num_aux * num_aux}"; 
+              
+            }
+            catch (Exception)
+            {
+                return "El valor ingresado es incorrecto";
+            }
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
